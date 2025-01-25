934f7dd8ec84ec534d3b0605ab0bb068393af9f0
Keerthi <keerthi.appasani1@gmail.com>
2025-01-25 16:30:59 +1100

    lottie file changes

--- src/components/ShowLottie.tsx
+++ src/components/ShowLottie.tsx
@@ -1,14 +1,22 @@
-import { Player } from '@lottiefiles/react-lottie-player';
+import { Player } from "@lottiefiles/react-lottie-player";
+import dynamic from "next/dynamic";
 
+const MyComponentNoSSR = dynamic(
+  () =>
+    import("@lottiefiles/react-lottie-player").then((module) => ({
+      default: module.Player,
+    })),
+  { ssr: false }
+);
 type Props = {
   path: any;
   className?: string;
 };
 
-const ShowLottie = ({ path, className = '' }: Props) => {
+const ShowLottie = ({ path, className = "" }: Props) => {
   return (
     <div className={`max-w-sm md:max-w-md ${className}`}>
-      <Player autoplay loop src={path}></Player>
+      <MyComponentNoSSR autoplay loop src={path}></MyComponentNoSSR>
     </div>
   );
 };
